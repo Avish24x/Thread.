8063fc71774d5a5c0d3916ccc0d74462c8b12a89
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-08-28 16:23:10 +0400

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="head-text text-left">home</h1>
+    </div>
+  );
+}
