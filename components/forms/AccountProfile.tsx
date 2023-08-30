d9f4e31df3a3acc455bfab7475d01252a5168b51
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-08-30 15:58:56 +0400

    added uploadthing api key and mongoDB

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import * as z from "zod";
 import Image from "next/image";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -36,6 +38,10 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+
+  const router = useRouter();
+  const pathname = usePathname();
+
   const form = useForm({
     resolver: zodResolver(Uservalidation),
     defaultValues: {
@@ -83,7 +89,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       }
     }
 
-    // TODO: UPDATE USER PROFILE
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
   };
 
   return (
