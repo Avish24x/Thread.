8a9f8a32a546dc1e9fcef4582564e43147bd9bd3
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-09-13 02:28:41 +0400

    last

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,18 +1,22 @@
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { fetchUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { profileTabs } from "@/constants";
-import Image from "next/image";
+
 import ThreadsTab from "@/components/shared/ThreadsTab";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
+
   const userInfo = await fetchUser(params.id);
-  //   will redirect the user if user not onboarded will go to onboarding page
   if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
     <section>
       <ProfileHeader
@@ -37,9 +41,10 @@ async function Page({ params }: { params: { id: string } }) {
                   className="object-contain"
                 />
                 <p className="max-sm:hidden">{tab.label}</p>
+
                 {tab.label === "Threads" && (
                   <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
-                    {userInfo?.threads?.length}
+                    {userInfo.threads.length}
                   </p>
                 )}
               </TabsTrigger>
@@ -51,6 +56,7 @@ async function Page({ params }: { params: { id: string } }) {
               value={tab.value}
               className="w-full text-light-1"
             >
+              {/* @ts-ignore */}
               <ThreadsTab
                 currentUserId={user.id}
                 accountId={userInfo.id}
@@ -63,5 +69,4 @@ async function Page({ params }: { params: { id: string } }) {
     </section>
   );
 }
-
 export default Page;
